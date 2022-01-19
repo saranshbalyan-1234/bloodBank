01a0dd783cd6e21b3b641067f61fa2601d768545
saransh balyan <saransh@kditsolutions.com>
2022-01-19 19:50:23 +0530

    swagger

---
+++ app/Http/Controllers/SwaggerController.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class SwaggerController extends Controller
+{
+  /**
+ * @OA\Info(title="Blood Bank API", version="v1")
+* @OA\Tag(
+*     name="Projects",
+*     description="API Endpoints of Projects"
+* )
+*/
+
+       /**
+ * @OA\Get(
+ *     path="/projects",
+ * tags={"Auth"},
+ *     @OA\Response(response="200", 
+ * description="Display a listing of projects.")
+ * )
+ */
+}
--- routes/api.php
+++ routes/api.php
@@ -5,7 +5,7 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\UsersController;
 use App\Http\Controllers\DonorsController;
 use App\Http\Controllers\ConstantController;
-
+use App\Http\Controllers\WorldController;
 /*
 |--------------------------------------------------------------------------
 | API Routes
@@ -21,14 +21,15 @@ Route::post('/login',[UsersController::class,'login']);
 Route::post('/register',[UsersController::class,'register']);
 
 Route::group(['middleware'=>['auth:sanctum']],function () {
-
     Route::post('/update',[UsersController::class,'update']);
     Route::post('/getDonorById',[UsersController::class,'getDonorById']);
     Route::post('/getAllDonors',[UsersController::class,'getAllDonors']);
     Route::post('/getAllDonorsByState',[UsersController::class,'getAllDonorsByState']);
     Route::post('/getAllDonorsByStateCity',[UsersController::class,'getAllDonorsByStateCity']);
     Route::post('/getAllBloodType',[ConstantController::class,'getAllBloodType']);
-    Route::post('/getAllStates',[ConstantController::class,'getAllStates']);
+    Route::post('/getAllCountries',[WorldController::class,'getAllCountries']);
+    Route::post('/getAllStatesByCountry',[WorldController::class,'getAllStatesByCountry']);
+    Route::post('/getAllCityByStates',[WorldController::class,'getAllCityByStates']);
     Route::post('/logout',[UsersController::class,'logout']);
   
 });
