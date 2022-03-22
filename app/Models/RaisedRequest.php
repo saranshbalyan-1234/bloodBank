ad40207c65c868629f11f2b55e9dce022a90064a
Shivam <codewrapperz360@gmail.com>
2022-03-23 00:56:57 +0530

    raiserequest

---
+++ app/Models/RaisedRequest.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class RaisedRequest extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'requester_id',
+        'donor_id'
+    
+    ];
+}
