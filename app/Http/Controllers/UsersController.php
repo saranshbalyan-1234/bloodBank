b408f84b1604afc56dbb4ba207bfcb5fca3d1b50
saransh balyan <saransh@kditsolutions.com>
2022-04-21 15:14:25 +0530

    .

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -131,6 +131,7 @@ class UsersController extends Controller
         if($req->city=='All'||$req->city==''||$req->city==null) $temp->forget('city'); 
         if($req->state=='All'||$req->state==''||$req->state==null) $temp->forget('state'); 
         if($req->district=='All'||$req->district==''||$req->district==null) $temp->forget('district'); 
+        if($req->pincode=='All'||$req->pincode==''||$req->pincode==null) $temp->forget('pincode'); 
         
         if($req->pincode && $req->pincode!=''||$req->pincode!==null){
           $req->district&& $temp->forget('district'); 
