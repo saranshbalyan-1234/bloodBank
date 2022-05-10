6cbe61229b516674ecf5152f35d05fac4ea88d7e
saransh balyan <saransh@kditsolutions.com>
2022-05-10 07:17:32 +0530

    .

--- app/Models/FeedBack.php
+++ app/Models/FeedBack.php
@@ -4,7 +4,7 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
-
+use Carbon\Carbon;
 class FeedBack extends Model
 {
     use HasFactory;
