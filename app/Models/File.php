c09e34655f807e2f280df7e0336527c8253ca280
saranshbalyan-1234 <57568571+saranshbalyan-1234@users.noreply.github.com>
2022-05-16 00:20:55 +0530

    Update File.php

--- app/Models/File.php
+++ app/Models/File.php
@@ -10,7 +10,7 @@ class File extends Model
     use HasFactory;
     protected $table='files';
     protected $fillable = [
-        'user_id','file'
+        'request_donors_id','file'
        ];
 }
 
